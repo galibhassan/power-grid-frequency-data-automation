552db6f75f18a48a6c1612d7c4ad9b5f3c2708d4
Galib Hassan <galib.hassan.0@gmail.com>
2020-06-17 00:40:46 +0200

    add csv to json converter, and some sample data

---
+++ csvToJson.py
@@ -0,0 +1,22 @@
+import json
+import pandas as pd
+
+csvSourcePath01 = './sampleCSV/2017_12.csv'
+csvSourcePath02 = './sampleCSV/2014_10.csv'
+csvSourcePath03 = './sampleCSV/2018_05.csv'
+
+
+csvPathArr = [csvSourcePath01, csvSourcePath02, csvSourcePath03]
+
+for i in range(len(csvPathArr)):
+    df = pd.read_csv(csvPathArr[i], names=['Time', 'Frequency'])
+    timeArr = df['Time']
+    freqArr = df['Frequency']
+
+    freqJson = open(f'./output/frequency_{i}.json', 'w+')
+    timeJson = open(f'./output/time_{i}.json', 'w+')
+
+    json.dump(freqArr.tolist(), freqJson)
+    json.dump(timeArr.tolist(), timeJson)
+    freqJson.close()
+    timeJson.close()
